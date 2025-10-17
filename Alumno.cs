ea3f7660f3164e1d091fba35d0b940c817b2ed1e
aguspignata35-jpg <aguspignata35@gmail.com>
2025-10-17 12:05:32 -0300

    Add Alumno class with properties and methods

--- Alumno.cs
+++ Alumno.cs
@@ -1,1 +1,51 @@
+using System;
+
+namespace ejemplo
+{
+	public class alumno
+	{
+		// Atributos privados
+		private string nombre;
+		private int legajo;
+		private double nota;
+		
+		// Constructor vacío la clase alumno
+		public Alumno()
+		{
+			// Inicializacion para ver los los string int y double 
+			this.nombre = "";
+			this.legajo = 0;
+			this.nota = 0.0;
+		}
+		
+		// Construimos los parametros 
+		public alumno(string nombre, int legajo, double nota):base(nombre, apellido, dni)
+		{
+			this.nombre = nombre;
+			this.legajo = legajo;
+			this.nota = nota;
+		}
+		
+		// Métodos públicos para leer los datos con get 
+		public string Getnombre()
+		{
+			return nombre;
+		}
+		
+		public int Getlegajo()
+		{
+			return legajo;
+		}
+		
+		public double Getnota()
+		{
+			return nota;
+		}
+		
+		public void mostrardatos()
+		{
+			Console.WriteLine("Alumno: " + nombre + " | Legajo: " + legajo + " | Nota: " + nota);
+		}
+	}
+}
 
